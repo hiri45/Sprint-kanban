3c1f7355c10da4ab6f946b6fe9d327cf28783308
ASJAD ARSHAD <mars0003@student.monash.edu>
2022-09-05 22:34:19 +1000

    Created add task function to add tasks into product backlog, changed display task function to update task with local storage info

--- Sandbox/SRC/shared.js
+++ Sandbox/SRC/shared.js
@@ -80,10 +80,20 @@ class itemList {
         return this._notes.length;
     }
 
-    addstickynotes(id) {
-        this._notes.push(new Stickynote(id));
+    addstickynotes(Stickynote) {
+        this._notes.push(Stickynote);
 
+    }
+    deletestickynotes(id){
+        for(let i = 0;i < this._notes.length;i++){
+            if(id==this._notes[i].id){
+                this._notes.splice(i,1);
+            }
+        }
 
+    }
+    getNote(index){
+        return this._notes[index];
     }
     fromData(data){
         let theData = data._notes;
@@ -120,11 +130,7 @@ if(toCheck === true){
     let theData = getDataLocalStorage();
     itemlist.fromData(theData);
 }
-else{
-    let new_id = gen_ID()
-    itemlist.addstickynotes(new_id); //create a locker with default id that is A001
-    updateLocalStorage(itemlist);
-}
+
 
 function gen_ID(){
     let gen_id = Math.random() * 1000
@@ -133,7 +139,8 @@ function gen_ID(){
 function displayNotes(data){
     let listnotes = '';
     for(let i = 0; i < data.count; i++) {
-        listnotes += "<div class='demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--4-col'><table class='mdl-data-table mdl-js-data-table' style='width: 100%;'><thead><tr><th class='mdl-data-table__cell--non-numeric mdl-cell--4-col'>Task 1</th><th style='text-align:right; padding-right: 0px;'><button id='task1' class='mdl-button mdl-js-button mdl-button--icon' style='Scale: 1;\'><i class='material-icons'>more_vert</i></button><ul class='mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect' htmlFor='task1'><li class='mdl-menu__item'>Edit Task</li><li class='mdl-menu__item'>Delete Task</li><li class='mdl-menu__item'>Move to Sprint 1</li><li disabled class='mdl-menu__item'>Disable button</li></ul></th></tr></thead><tbody onClick=''><tr style='width: 100%';><td style='text-align:left'>Description:</td><td style='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>Some description</td></tr><tr style='width: 100%;'><td style='text-align:left'>Tags:</td><td style='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>Some tag</td></tr><tr style='width: 100%;'><td style='text-align:left'>Priority:</td><td style='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>High</td></tr><tr style='width: 100%;'><td style='text-align:left'>Story Point:</td><td class='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>Some here</td></tr></tbody></table></div>"
+        let id = data._notes.id
+        listnotes += "<div class='demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--4-col'><table class='mdl-data-table mdl-js-data-table' style='width: 100%;'><thead><tr><th class='mdl-data-table__cell--non-numeric mdl-cell--4-col'>"+data._notes[i].name+"</th><th style='text-align:right; padding-right: 0px;'><button id="+data._notes[i].name+" class='mdl-button mdl-js-button mdl-button--icon' style='Scale: 1;\'><i class='material-icons'>more_vert</i></button><ul class='mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect' for="+data._notes[i].name+"><li class='mdl-menu__item'>Edit Task</li><li class='mdl-menu__item' onclick='deleteTask()'>Delete Task</li><li class='mdl-menu__item'>Move to Sprint 1</li><li disabled class='mdl-menu__item'>Disable button</li></ul></th></tr></thead><tbody onClick=''><tr style='width: 100%;'><td style='text-align:left'>Tags:</td><td style='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>"+data._notes[i].tag+"</td></tr><tr style='width: 100%;'><td style='text-align:left'>Priority:</td><td style='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>"+data._notes[i].priority+"</td></tr><tr style='width: 100%;'><td style='text-align:left'>Story Point:</td><td class='text-align:right; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;'>"+data._notes[i].storypoint+"</td></tr><tr style='width: 100%';><td style='text-align:Left'>Click Here to Expand</td></tr></tbody></table></div>"
     }
 
     let outputArea = document.getElementById("NoteDisplay");
@@ -142,4 +149,35 @@ function displayNotes(data){
 
 displayNotes(itemlist)
 
+function addTask(){
+    let task_name = document.getElementById('task_name');
+    let task_description = document.getElementById('task_description');
+    let task_tags = document.getElementById('task_tags');
+    let task_priority = document.getElementById('task_priority');
+    let task_storypoint = document.getElementById('task_story_point');
+
+
+    let task = new Stickynote(gen_ID())
+    task.description = task_description.value;
+    task.name = task_name.value;
+    task.tag =task_tags.value;
+    task.priority =task_priority.value;
+    task.storypoint = task_storypoint.value;
+    itemlist.addstickynotes(task)
+    updateLocalStorage(itemlist)
+    window.location = "index.html"
+
+
+}
+function deleteTask(){
+    let toConfirm = confirm("Press OK to delete this task.") //to confirm if the user want to delete the locker
+    if (toConfirm===true){ //if it's true
+        id =
+       itemlist.deletestickynotes(id);
+       updateLocalStorage(itemlist);
+       alert("This task has been deleted.");
+       window.location="index.html";
+    } //if the user do not confirm, do nothing
+
+}
 
